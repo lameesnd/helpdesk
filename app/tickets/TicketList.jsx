cdda83bd01ae28fb2e155d3cfeb57f4826c87547
lamees-nd <lameesn@equitick.com>
2024-06-25 14:44:15 +0300

    Fetching & Revalidating Data

---
+++ app/tickets/TicketList.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+async function getTickets() {
+  const res = await fetch("http://localhost:4000/tickets", {
+    next: { revalidate: 0 }, //refetch data after N seconds if refresh is within the next N seconds, else it will serve data from the cache. if set to 0, it will always fetch data
+  });
+
+  return res.json();
+}
+
+export default async function TicketList() {
+  const tickets = await getTickets();
+
+  return (
+    <>
+      {tickets.map((ticket) => (
+        <div key={ticket.id} className="card my-5">
+          <h3>{ticket.title}</h3>
+          <p>{ticket.body.slice(0, 200)} ...</p>
+          <div className={`pill ${ticket.priority}`}>
+            {ticket.priority} Priority
+          </div>
+        </div>
+      ))}
+      {tickets.length === 0 && <p>No Open tickets!</p>}
+    </>
+  );
+}
--- app/tickets/page.jsx
+++ app/tickets/page.jsx
@@ -1,9 +1,18 @@
 import React from "react";
+import TicketList from "./TicketList";
 
 export default function Tickets() {
   return (
     <main>
-      <div>Tickets</div>
+      <nav>
+        <div>
+          <h2>Tickets</h2>
+          <p>
+            <small>Currently open tickets...</small>
+          </p>
+        </div>
+      </nav>
+      <TicketList />
     </main>
   );
 }
