7922d87290a2106f13e1bbc6d9c8f56d67c86b18
lamees-nd <lameesn@equitick.com>
2024-06-26 13:17:44 +0300

    Loading UI & Suspense

--- app/tickets/TicketList.jsx
+++ app/tickets/TicketList.jsx
@@ -2,6 +2,8 @@ import Link from "next/link";
 import React from "react";
 
 async function getTickets() {
+  //imitate delay to show loader
+  await new Promise((resolve) => setTimeout(resolve, 3000));
   const res = await fetch("http://localhost:4000/tickets", {
     next: { revalidate: 0 }, //refetch data after N seconds if refresh is within the next N seconds, else it will serve data from the cache. if set to 0, it will always fetch data
   });
